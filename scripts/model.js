c792ceb69127a8306eefe9e40508b94543cc3c3a
FND <fnd@oda.(none)>
2009-11-21 17:52:12 +0000

    s/tiddler/mico/g
    also s/Store/Collection/g

--- scripts/model.js
+++ scripts/model.js
@@ -1,38 +1,38 @@
 /*
- * content item
+ * unit of micro-content
  */
 
-var Tiddler = function(title) {
+var Mico = function(title) {
 	this.id = generateID();
 	this.title = title;
 };
 
 /*
- * tiddler collection
+ * collection of MicoS
  */
 
-var Store = function(tiddlers) {
-	tiddlers = tiddlers || [];
+var Collection = function(micos) {
+	micos = micos || [];
 	this.index = {};
 	this.linkmap = {}; // XXX: rename?
-	for(var i = 0; i < tiddlers.length; i++) {
-		this.add(tiddlers[i]);
+	for(var i = 0; i < micos.length; i++) {
+		this.add(micos[i]);
 	}
-	this.length = tiddlers.length; // pseudo-array
+	this.length = micos.length; // pseudo-array
 };
 
-Store.prototype = new Array();
+Collection.prototype = new Array();
 
-Store.prototype.add = function(tiddler) {
-	this.push(tiddler);
-	this.index[tiddler.id] = this.length - 1;
-	if(this.linkmap[tiddler.title] === undefined) {
-		this.linkmap[tiddler.title] = [];
+Collection.prototype.add = function(mico) {
+	this.push(mico);
+	this.index[mico.id] = this.length - 1;
+	if(this.linkmap[mico.title] === undefined) {
+		this.linkmap[mico.title] = [];
 	}
-	this.linkmap[tiddler.title].push(tiddler.id);
+	this.linkmap[mico.title].push(mico.id);
 };
 
-Store.prototype.remove = function(id) {
+Collection.prototype.remove = function(id) {
 	var i = this.index[id];
 	if(i === undefined) {
 		return false;
@@ -40,24 +40,24 @@ Store.prototype.remove = function(id) {
 	var title = this[i].title;
 	delete this.index[id];
 	delete this.linkmap[title];
-	var tiddler = this.splice(i, 1)[0];
+	var mico = this.splice(i, 1)[0];
 	for(var key in this.index) { // update index references -- XXX: inefficient!?
 		if(this.index[key] > i) {
 			this.index[key]--;
 		}
 	}
-	return tiddler;
+	return mico;
 };
 
 // XXX: experimental
-Store.prototype.filter = function(field, value, invert) {
+Collection.prototype.filter = function(field, value, invert) {
 	if(invert) {
 		var check = function(a, b) { return a !== b; };
 	} else {
 		check = function(a, b) { return a === b; };
 	}
-	var matches = jQuery.map(this, function(tiddler, i) { // XXX: use Array.filter to avoid dependency
-		return check(tiddler[field], value) ? tiddler : null;
+	var matches = jQuery.map(this, function(mico, i) { // XXX: use Array.filter to avoid dependency
+		return check(mico[field], value) ? mico : null;
 	});
-	return new Store(matches);
+	return new Collection(matches);
 };
